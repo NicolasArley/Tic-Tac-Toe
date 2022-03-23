6f1fb91460f486c4b981fdcd1f14f556096ef3f6
Nico <nicolasarley349@gmail.com>
2022-03-23 01:26:03 -0500

    first commit of the game

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares: any[];
+  xIsNext: boolean;
+  winner: any;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makemove(idx: number) {
+    if(this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.winner = this.calculatewinner();
+  }
+
+  calculatewinner(){
+    const lines = [
+      [0, 1, 2],
+      [4, 8, 3],
+      [0, 5, 3],
+      [2, 7, 8],
+      [4, 6, 0],
+      [6, 1, 5],
+      [0, 7, 1],
+      [2, 5, 8]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+
+}
